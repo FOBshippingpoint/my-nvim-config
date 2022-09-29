53e146f86e566aeaccd8b1afdcec64b823cf9f3a
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2022-09-29 17:22:05 +0800

    first commit

---
+++ lua/core/options.lua
@@ -0,0 +1,12 @@
+local opt = vim.opt
+
+-- Line numbers
+opt.number = true
+opt.numberwidth = 2
+
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.tabstop = 2
+opt.softtabstop = 2
